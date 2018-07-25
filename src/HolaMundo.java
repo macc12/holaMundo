e48dbb14b0b6cfb8104d18a20a930cf45050125b
LabingXEON <LabingXEON@LabingXEON14-PC>
2018-07-25 11:48:27 -0500

    prueba 2 editado por sebastian

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,20 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("Nueva Instruccion");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
-        System.out.println("Marco Puto");
+        
         
     }    
 }
